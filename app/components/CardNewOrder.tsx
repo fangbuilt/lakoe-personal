c7c89de4ae7fbfcaef4b67bcac18f7eaf7e8a518
pujaayumiswari <payumiswari@gmail.com>
2023-09-05 14:24:23 +0700

    feat: fetching BITESHIP_API within pop-up modal at NewOrder Component to send Courier Request

--- app/components/CardNewOrder.tsx
+++ app/components/CardNewOrder.tsx
@@ -1,26 +1,27 @@
 import {
-  Card,
-  Image,
-  CardBody,
-  Text,
-  Heading,
-  Button,
   Box,
+  Button,
+  Card,
+  CardBody,
   Divider,
-  useDisclosure,
+  Heading,
+  Image,
   Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
   ModalBody,
+  ModalCloseButton,
+  ModalContent,
   ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const data = [
   {
     id: '1',
-    title: ' CREWNECK BASIC-BLACK | sweeter polos hodie polos crewneck - S',
+    title: 'CREWNECK BASIC-BLACK | sweeter polos hodie polos crewneck - S',
     quantity: '2',
     price: '190.000',
     invoice: 'INV/20230809/MPL/00003432',
@@ -29,9 +30,37 @@ const data = [
       'https://images.unsplash.com/photo-1434389677669-e08b4cac3105?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60',
   },
 ];
+
 export default function CardProducts(props: any) {
+  const [selectedStatus, setSelectedStatus] = useState<string | null>(null);
+
   const { isOpen, onOpen, onClose } = useDisclosure();
-  console.log();
+
+  // Fetch API
+  const sendDataToAPI = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('status', selectedStatus || '');
+
+      const res = await fetch('https://api.biteship.com/v1/rates/couriers', {
+        method: 'POST',
+        headers: {
+          authorization: process.env.BITESHIP_API as string,
+        },
+        body: formData,
+      });
+
+      if (res.ok) {
+        alert('Data berhasil dikirim');
+      } else {
+        //
+        alert('Gagal mengirim data');
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <>
       <Box>
@@ -40,51 +69,49 @@ export default function CardProducts(props: any) {
             key={card.id}
             overflow="hidden"
             variant="outline"
-            display={'flex'}
-            justifyContent={'space-between'}
-            margin={'50px 5% 10px'}
+            display="flex"
+            justifyContent="space-between"
+            margin="50px 5% 10px"
           >
-            {/* atas */}
-            <Box
-              display={'flex'}
-              justifyContent={'space-between'}
-              padding={'15px'}
-            >
+            {/* Atas */}
+            <Box display="flex" justifyContent="space-between" padding="15px">
               <Box>
                 <Button
-                  padding={'4px 8px'}
-                  borderRadius={'4px'}
-                  backgroundColor={'#008F5D'}
-                  fontSize={'14px'}
-                  fontWeight={'400'}
-                  size={'sm'}
+                  padding="4px 8px"
+                  borderRadius="4px"
+                  backgroundColor="#008F5D"
+                  fontSize="14px"
+                  fontWeight="400"
+                  size="sm"
                   mb={2}
-                  color={'white'}
+                  color="white"
                 >
                   {card.status}
                 </Button>
-
                 <Text
-                  fontSize={'14px'}
-                  fontWeight={'500'}
-                  lineHeight={'16px'}
-                  color={'#909090'}
+                  fontSize="14px"
+                  fontWeight="500"
+                  lineHeight="16px"
+                  color="#909090"
                 >
                   {card.invoice}
                 </Text>
               </Box>
               <Box
-                display={'flex'}
-                justifyContent={'center'}
-                flexDirection={'column'}
+                display="flex"
+                justifyContent="center"
+                flexDirection="column"
               >
                 <Button
-                  borderRadius={'15px'}
-                  padding={'4px 12px'}
-                  border={'1px solid #D5D5D5'}
-                  size={'sm'}
-                  bg={'transparent'}
-                  onClick={onOpen}
+                  borderRadius="15px"
+                  padding="4px 12px"
+                  border="1px solid #D5D5D5"
+                  size="sm"
+                  bg="transparent"
+                  onClick={() => {
+                    setSelectedStatus(card.status);
+                    onOpen();
+                  }}
                 >
                   Proses Pesanan
                 </Button>
@@ -96,57 +123,51 @@ export default function CardProducts(props: any) {
                     <ModalBody>
                       <Text>Pilih proses selanjutnya :</Text>
                     </ModalBody>
-
                     <ModalFooter>
-                      <Button colorScheme="blue" mr={3} onClick={onClose}>
+                      <Button colorScheme="blue" mr={3} onClick={sendDataToAPI}>
                         Selesai Packing
                       </Button>
-                      <Button variant="ghost">Siap dikirim</Button>
+                      <Button variant="ghost" onClick={onClose}>
+                        Siap dikirim
+                      </Button>
                     </ModalFooter>
                   </ModalContent>
                 </Modal>
               </Box>
             </Box>
-
-            <Divider w={'100%'} />
-
-            {/* bawah */}
-            <Box
-              display={'flex'}
-              justifyContent={'space-between'}
-              padding={'15px'}
-            >
-              <Box display={'flex'}>
+            <Divider w="100%" />
+            {/* Bawah */}
+            <Box display="flex" justifyContent="space-between" padding="15px">
+              <Box display="flex">
                 <Box
-                  display={'flex'}
-                  justifyContent={'center'}
-                  flexDirection={'column'}
+                  display="flex"
+                  justifyContent="center"
+                  flexDirection="column"
                 >
                   <Image
                     objectFit="cover"
-                    width={'52px'}
-                    height={'52px'}
-                    src=" {card.picture}"
+                    width="52px"
+                    height="52px"
+                    src={card.picture}
                     alt="brown clothes"
-                    borderRadius={'8px'}
+                    borderRadius="8px"
                   />
                 </Box>
-
                 <Box>
                   <CardBody>
                     <Heading
                       size="md"
-                      fontSize={'16px'}
-                      lineHeight={'20px'}
-                      fontWeight={'700'}
+                      fontSize="16px"
+                      lineHeight="20px"
+                      fontWeight="700"
                     >
                       {card.title}
                     </Heading>
                     <Text
                       py="2"
-                      fontSize={'14px'}
-                      color={'#909090'}
-                      lineHeight={'16px'}
+                      fontSize="14px"
+                      color="#909090"
+                      lineHeight="16px"
                     >
                       {card.quantity} Barang
                     </Text>
@@ -154,20 +175,20 @@ export default function CardProducts(props: any) {
                 </Box>
               </Box>
               <Box
-                justifyContent={'center'}
-                display={'flex'}
-                flexDirection={'column'}
-                flex={'end'}
+                justifyContent="center"
+                display="flex"
+                flexDirection="column"
+                flex="end"
               >
                 <Text
-                  fontSize={'14px'}
-                  fontWeight={'500'}
-                  color={'#909090'}
-                  lineHeight={'16px'}
+                  fontSize="14px"
+                  fontWeight="500"
+                  color="#909090"
+                  lineHeight="16px"
                 >
                   Total Belanja
                 </Text>
-                <Text fontSize={'14px'} fontWeight={'700'}>
+                <Text fontSize="14px" fontWeight="700">
                   {card.price}
                 </Text>
               </Box>
