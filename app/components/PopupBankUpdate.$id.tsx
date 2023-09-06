0d9b769ae1a89e6b42892619511ce86ebc52f5e8
rhmtrizky <rahmatrizkyrifai@gmail.com>
2023-09-06 20:24:48 +0700

    feat: crud

--- app/components/PopupBankUpdate.$id.tsx
+++ app/components/PopupBankUpdate.$id.tsx
@@ -17,12 +17,7 @@ import { Form } from '@remix-run/react';
 import React from 'react';
 import { BsBank } from 'react-icons/bs';
 import { BiEditAlt } from 'react-icons/bi';
-import { getBankList } from '~/modules/dashboard/dashboard.service';
-import { SelectBankOptionUpdate } from './selectBankOptionUpdate';
-
-export async function loader() {
-  return await getBankList();
-}
+import { SelectBankOptionUpdate } from './selectBankOption';
 
 export default function UpdateBank(props: any) {
   const { id } = props;
--- app/components/selectBankOption.tsx
+++ app/components/selectBankOption.tsx
@@ -17,3 +17,37 @@ export function SelectBankOption() {
     />
   );
 }
+
+export function SelectBankOptionUpdate() {
+  return (
+    <CreatableSelect
+      name="updateBankName"
+      placeholder="Jenis Bank"
+      isClearable
+      options={options}
+    />
+  );
+}
+
+export function SelectRekening({ dataBank }: any) {
+  if (!dataBank || dataBank.length === 0) {
+    return 'ga dapet bank';
+  }
+
+  return (
+    <>
+      {dataBank.map((data: any) => (
+        <CreatableSelect
+          key={data.id}
+          placeholder="Pilih Rekening Anda"
+          options={[
+            {
+              label: `${data.bank_name} - ${data.name} - ${data.bank_number}`,
+              value: `${data.bank_name}-${data.name}-${data.bank_number}`,
+            },
+          ]}
+        />
+      ))}
+    </>
+  );
+}
