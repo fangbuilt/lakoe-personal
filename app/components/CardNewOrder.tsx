4ba0f394927e2dbc7699b770723fbe899961315f
unknown <aderino.2232510384@gmail.com>
2023-09-12 15:38:01 +0700

    fix: fix update

--- app/components/CardNewOrder.tsx
+++ app/components/CardNewOrder.tsx
@@ -1,7 +1,150 @@
-import { Box, Button, Card, Flex, Img, Text } from '@chakra-ui/react';
-import { Link } from '@remix-run/react';
+import {
+  Box,
+  Button,
+  Card,
+  Flex,
+  Img,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { UseSearch } from '~/hooks/useSearchOrder';
 import type { IOrderList } from '~/interfaces/order';
 export default function CardNewOrder(props: IOrderList) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { filteredOrders } = UseSearch();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleBalanceNotif = async () => {
+    try {
+      const mailerBaseUrl = 'https://connect.mailerlite.com';
+      const mailerEndPoint = '/api/subscribers';
+      const mailerApiKey =
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiM2E4ZjZkNTMxMDdkY2M1MjZjM2M5YTQxY2JhMjg0ZjJlOTc5NmFjOTA2MjVkMzRjN2I5NTVmNDY1ODlkZjcxOGM5NzY5ZmYyMzU5OTcxZTkiLCJpYXQiOjE2OTQxNTU1NDQuMTI1MzUyLCJuYmYiOjE2OTQxNTU1NDQuMTI1MzU0LCJleHAiOjQ4NDk4MjkxNDQuMTIwNDQsInN1YiI6IjYxNDY4NSIsInNjb3BlcyI6W119.KgsXIIo-rqViucL5U0QTHaG-Nhp0YJn0c752CSW1taUIVgfP0Dyk-vL-mHEGCLWl4CROGPwtzGakauaIGV1A-ijvg_16vEz04u8xKRzzuP4F9Hza78RnhTXjewo6oEiB4_E3WwFU6qalQmzoNaSzmaBI4zi6HZOO29uEHtZRswRfmi5g1XmDyqo2SmaL6S3nTU7xMoHaBlvY7UnanzqdpX0nr-nxS-05ADZRlo1a3YDQBihDFLzrhN8xgtXipU5O7nz18-Ivpj2TNjaMNk85zZukLYPxF1lVXrbNFWKVWJKMk9gthqMWsPDQTg7GexZSE-0uzZL8CO1azw_hCdJUJQYM3KYw1pb6PUm4YSO-Br4etsClpICaivipa5EGSOKF3wvAhyHa12ZIZuJcBadQPyAaiDi8a0s1O6UbLMBa_45oDDfeNQsEpXg9i5hkAe7H0DEdgM69JMh0zmu4Vi8s3f_fmz0pfGjXfKVT6g0KHx0K6AYhN714R2x6FOB-au4QrPlE_UdvIOO959uozJ4CHHiBKClWcTLRELWwCPmo6y5s-K8_s7h1czfV2MVx5mfihABiLyxCv3y6EwxgTi6gjKiN4NcCMoGnxt0dwPos67QQ-gRn2SdQoN0rsrKGuZltLOBza1cnqoHAZAFHiSrJq332VNoJhNuXN-3MoXw1LCY'; //Rino - Puja (desi123321123321@gmail.com)
+
+      const mailerData = {
+        email: 'miswaripujaayu+007unch@gmail.com',
+        fields: {
+          company: 'ADD MORE BALANCE', //company berperan sebagai "title" dalam mailerlite
+          last_name:
+            "you need to add more balance to your platform system so that your sellers can keep sending packages to their customer without being delayed just because you're lack of money. do what you gotta do", //last_name berperan sebagai isian pesan ("message") dalam mailerlite
+        },
+        groups: ['98713000939095999'],
+      };
+
+      const mailerRequest = {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${mailerApiKey}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(mailerData),
+      };
+
+      const response = await fetch(
+        `${mailerBaseUrl}${mailerEndPoint}`,
+        mailerRequest
+      );
+      const responseData = await response.json();
+      console.log('Data Email :', responseData);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const systembalance = 100; //saldo LAKOE
+
+  const afterpacking = () => {
+    if (systembalance > 50000) {
+      handleOrderCourier();
+    } else {
+      handleBalanceNotif();
+    }
+  };
+
+  const handleOrderCourier = async () => {
+    try {
+      const baseUrl = 'https://api.biteship.com';
+      const endpoint = '/v1/orders';
+      const apiKey =
+        'biteship_test.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYml0ZXNoaXBMYWtvZSIsInVzZXJJZCI6IjY0ZjU4ZjdiZWJlNjI2M2RiOWY5MWYxMCIsImlhdCI6MTY5NDA3MjA0N30.t-4Rg4MSvhx6Uq9bKhVlo2DFPvb3L9jmObDCwFzSuuk'; //Rino - Puja
+
+      const orderData = {
+        shipper_contact_name: 'megakuningan',
+        shipper_contact_phone: '081277882932',
+        shipper_contact_email: 'biteship@test.com',
+        shipper_organization: 'Biteship Org Test',
+        origin_contact_name: 'megakuningan',
+        origin_contact_phone: '081740781720',
+        origin_address: 'Plaza Senayan, Jalan Asia Afrika...',
+        origin_note: 'Deket pintu masuk STC',
+        origin_coordinate: {
+          latitude: -6.2253114,
+          longitude: 106.7993735,
+        },
+        origin_postal_code: 12440,
+        destination_contact_name: 'stevanus miswari',
+        destination_contact_phone: '08170032123',
+        destination_contact_email: 'jon@test.com',
+        destination_address: 'Lebak Bulus MRT...',
+        destination_postal_code: 12950,
+        destination_note: 'Near the gas station',
+        destination_cash_proof_of_delivery: true,
+        destination_coordinate: {
+          latitude: -6.28927,
+          longitude: 106.77492000000007,
+        },
+        courierName: 'grab',
+        courierService: '',
+        courier_insurance: 500000,
+        delivery_type: 'later',
+        delivery_date: '2024-09-24',
+        delivery_time: '12:00',
+        order_note: 'Please be careful',
+        metadata: {},
+        items: [
+          {
+            id: '5db7ee67382e185bd6a14608',
+            name: 'Black L',
+            image: '',
+            description: 'White Shirt',
+            value: 165000,
+            quantity: 1,
+            height: 10,
+            length: 10,
+            weight: 200,
+            width: 10,
+          },
+        ],
+      };
+
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(orderData),
+      };
+
+      const response = await fetch(`${baseUrl}${endpoint}`, requestOptions);
+      const responseData = await response.json();
+
+      alert(responseData);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const [modalText, setModalText] = useState('');
+
   return (
     <>
       <Card mb={5} boxShadow={'xs'}>
@@ -21,17 +164,60 @@ export default function CardNewOrder(props: IOrderList) {
                 </Button>
 
                 {/* SET WHAT DO YOU WANT TO DO WITH YOUR BUTTON HERE */}
-                <Link to={`http://wa.me/${props.telephone}`}>
-                  <Button
-                    bg={'transparent'}
-                    border={'1px solid #D5D5D5'}
-                    borderRadius={'full'}
-                    fontSize={'14px'}
-                  >
-                    Proses Pesanan
-                  </Button>
-                </Link>
+                <Button
+                  bg={'transparent'}
+                  border={'1px solid #D5D5D5'}
+                  borderRadius={'full'}
+                  fontSize={'14px'}
+                  onClick={() => {
+                    setModalText('Apakah sudah di pack dan siap dikirim?');
+                    onOpen();
+                  }}
+                >
+                  Proses Pesanan
+                </Button>
                 {/*  */}
+                {/* Modal */}
+                <Modal
+                  blockScrollOnMount={false}
+                  isOpen={isOpen}
+                  onClose={() => {
+                    setModalText('');
+                    onClose();
+                  }}
+                >
+                  <ModalOverlay />
+                  <ModalContent>
+                    <ModalHeader>Proses Pesanan</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                      <Text fontWeight="bold" mb="1rem">
+                        {modalText}
+                      </Text>
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button
+                        colorScheme="blue"
+                        mr={3}
+                        onClick={() => {
+                          setModalText('');
+                          onClose();
+                        }}
+                      >
+                        Cancel
+                      </Button>
+                      <Button
+                        variant="ghost"
+                        onClick={() => {
+                          afterpacking();
+                          onClose();
+                        }}
+                      >
+                        Selesai di Packing
+                      </Button>
+                    </ModalFooter>
+                  </ModalContent>
+                </Modal>
               </Flex>
               <Text my={1} fontSize={'14px'} color={'gray.400'} px={2}>
                 {props.invoice}
@@ -82,5 +268,3 @@ export default function CardNewOrder(props: IOrderList) {
     </>
   );
 }
-
-//test
