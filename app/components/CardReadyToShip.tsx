3e5e7c4909ad3585b5d0b067d2d0a84a176d4c06
bagasngidiw <bagasvanbacdim@gmail.com>
2023-09-13 16:17:02 +0700

    chore: delete unused fetch data

--- app/components/CardReadyToShip.tsx
+++ app/components/CardReadyToShip.tsx
@@ -1,12 +1,11 @@
-import { Box, Button, Card, Flex, Img, Text } from "@chakra-ui/react";
-import { useState } from "react";
-import type { IOrderList } from "~/interfaces/order";
-import ModalTracking from "./OrderTrackingModal";
-import getBiteshipTracking from "~/hooks/useOrderTracking";
+import { Box, Button, Card, Flex, Img, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+import type { IOrderList } from '~/interfaces/order';
+import ModalTracking from './orderTrackingModal';
 
 export default function ReadyToShipCard(props: IOrderList) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const {fetchData} = getBiteshipTracking()
+  // const {fetchData} = getBiteshipTracking()
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -19,32 +18,29 @@ export default function ReadyToShipCard(props: IOrderList) {
   return (
     <>
       {/* CARD START HERE */}
-      <Card mb={5} boxShadow={"xs"}>
+      <Card mb={5} boxShadow={'xs'}>
         <Box>
           <Box mt={5}>
             <Box>
-              <Flex justifyContent={"space-between"} px={2}>
+              <Flex justifyContent={'space-between'} px={2}>
                 <Button
-                  bg={"#147AF3"}
-                  color={"white"}
-                  fontWeight={"bold"}
+                  bg={'#147AF3'}
+                  color={'white'}
+                  fontWeight={'bold'}
                   colorScheme="gray.600"
-                  size={"sm"}
-                  pointerEvents={"none"}
+                  size={'sm'}
+                  pointerEvents={'none'}
                 >
                   Siap Dikirim
                 </Button>
                 <Box>
                   {/* SET WHAT DO YOU WANT TO DO WITH YOUR BUTTON HERE */}
                   <Button
-                    bg={"transparent"}
-                    border={"1px solid #D5D5D5"}
-                    borderRadius={"full"}
-                    fontSize={"14px"}
-                    onClick= {() => {
-                      openModal(),
-                      fetchData()
-                    }}
+                    bg={'transparent'}
+                    border={'1px solid #D5D5D5'}
+                    borderRadius={'full'}
+                    fontSize={'14px'}
+                    onClick={openModal}
                   >
                     Tracking Pengiriman
                   </Button>
@@ -52,44 +48,44 @@ export default function ReadyToShipCard(props: IOrderList) {
                   <ModalTracking isOpen={modalIsOpen} onClose={closeModal} />
                 </Box>
               </Flex>
-              <Text my={1} fontSize={"14px"} color={"gray.400"} px={2}>
+              <Text my={1} fontSize={'14px'} color={'gray.400'} px={2}>
                 {props.invoice}
               </Text>
               <hr />
-              <Flex justifyContent={"space-between"}>
-                <Box display={"flex"} w={"80%"}>
+              <Flex justifyContent={'space-between'}>
+                <Box display={'flex'} w={'80%'}>
                   <Img
-                    w={"52px"}
-                    h={"52px"}
-                    display={"inline"}
+                    w={'52px'}
+                    h={'52px'}
+                    display={'inline'}
                     src={props.imageProduct}
                     mt={3}
                   />
                   <Text
                     mt={4}
                     id="fm500"
-                    fontSize={"16px"}
-                    textOverflow={"ellipsis"}
-                    overflow={"hidden"}
-                    whiteSpace={"nowrap"}
-                    fontWeight={"700"}
+                    fontSize={'16px'}
+                    textOverflow={'ellipsis'}
+                    overflow={'hidden'}
+                    whiteSpace={'nowrap'}
+                    fontWeight={'700'}
                   >
                     {props.title}
-                    <Text color={"gray.400"} pb={3} fontWeight={"normal"}>
+                    <Text color={'gray.400'} pb={3} fontWeight={'normal'}>
                       1 Barang
                     </Text>
                   </Text>
                 </Box>
-                <Box mt={4} w={"15%"}>
+                <Box mt={4} w={'15%'}>
                   <Flex gap={1}>
-                    <Text color={"#909090"} fontSize={"14px"}>
+                    <Text color={'#909090'} fontSize={'14px'}>
                       Total
                     </Text>
-                    <Text color={"#909090"} fontSize={"14px"}>
+                    <Text color={'#909090'} fontSize={'14px'}>
                       Belanja
                     </Text>
                   </Flex>
-                  <Text fontWeight={"bold"} fontSize={"14px"}>
+                  <Text fontWeight={'bold'} fontSize={'14px'}>
                     Rp {props.totalAmount}
                   </Text>
                 </Box>
