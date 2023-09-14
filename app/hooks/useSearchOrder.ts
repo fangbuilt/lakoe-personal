d4e5e0181786545e082b515dd5af75f3b9dadcf0
pujaayumiswari <payumiswari@gmail.com>
2023-09-14 10:35:12 +0700

    feat: update neworder

--- app/hooks/useSearchOrder.ts
+++ app/hooks/useSearchOrder.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import data from '../tests/utils/dummy.json';
-=======
 import data from '../utils/dummy.json';
->>>>>>> 0aa73c4d167d6e7d3ba456e0ea46edffec25ee63
 import { useState, useEffect } from 'react';
 
 export function UseSearch() {
