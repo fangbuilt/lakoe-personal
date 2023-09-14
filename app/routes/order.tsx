a0cec8bb216ec1ae4627e2c2d2a27e9585eddaa3
unknown <aderino.2232510384@gmail.com>
2023-09-14 14:51:30 +0700

    feat: backend updated

--- app/routes/order.tsx
+++ app/routes/order.tsx
@@ -1,8 +1,19 @@
 import { Flex } from '@chakra-ui/react';
 import NavOrder from '~/layouts/NavOrder';
 import { ImplementGrid } from '~/layouts/Grid';
+import { getInvoiceByStatus } from '~/modules/order/order.service';
+import { useLoaderData } from '@remix-run/react';
+
+export async function loader() {
+  const dataInvoice = await getInvoiceByStatus();
+  console.log('test', dataInvoice);
+
+  return dataInvoice;
+}
 
 export default function Order() {
+  const data = useLoaderData<typeof loader>();
+  console.log(data);
   return (
     <ImplementGrid>
       <Flex align={'center'} justify={'center'} h={'100vh'}>
