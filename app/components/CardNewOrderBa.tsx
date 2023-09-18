6c71435f54403cfcea0353e9106c594ffec6470c
unknown <aderino.2232510384@gmail.com>
2023-09-18 08:15:55 +0700

    feat: update

--- app/components/CardNewOrderBa.tsx
+++ app/components/CardNewOrderBa.tsx
@@ -1,8 +1,148 @@
-import { Box, Button, Card, Flex, Image, Text } from '@chakra-ui/react';
-import { Link } from '@remix-run/react';
+import {
+  Box,
+  Button,
+  Card,
+  Flex,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { UseSearch } from '~/hooks/useSearchOrder copy';
 import type { IOrderDetailInvoice } from '~/interfaces/orderDetail';
 
 export default function CardNewOrderBa(props: IOrderDetailInvoice) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { filteredOrders } = UseSearch();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleBalanceNotif = async () => {
+    try {
+      const mailerBaseUrl = 'https://connect.mailerlite.com';
+      const mailerEndPoint = '/api/subscribers';
+      const mailerApiKey = process.env.MAILERLITE_API;
+
+      const mailerData = {
+        email: 'miswaripujaayu+123qowiej@gmail.com',
+        fields: {
+          company: 'ADD MORE BALANCE', //company berperan sebagai "title" dalam mailerlite
+          last_name:
+            "you need to add more balance to your platform system so that your sellers can keep sending packages to their customer without being delayed just because you're lack of money. do what you gotta do", //last_name berperan sebagai isian pesan ("message") dalam mailerlite
+        },
+        groups: ['98713000939095999'],
+      };
+
+      const mailerRequest = {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${mailerApiKey}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(mailerData),
+      };
+
+      const response = await fetch(
+        `${mailerBaseUrl}${mailerEndPoint}`,
+        mailerRequest
+      );
+      const responseData = await response.json();
+      console.log('Data Email :', responseData);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const systembalance = 100000; //saldo LAKOE
+
+  const afterpacking = () => {
+    if (systembalance > 50000) {
+      handleOrderCourier();
+    } else {
+      handleBalanceNotif();
+    }
+  };
+
+  const handleOrderCourier = async () => {
+    try {
+      const baseUrl = 'https://api.biteship.com';
+      const endpoint = '/v1/orders';
+      const apiKey = process.env.BITESHIP_API;
+
+      const orderData = {
+        shipper_contact_name: 'megakuningan',
+        shipper_contact_phone: '081277882932',
+        shipper_contact_email: 'biteship@test.com',
+        shipper_organization: 'Biteship Org Test',
+        origin_contact_name: 'megakuningan',
+        origin_contact_phone: '081740781720',
+        origin_address: 'Plaza Senayan, Jalan Asia Afrika...',
+        origin_note: 'Deket pintu masuk STC',
+        origin_coordinate: {
+          latitude: -6.2253114,
+          longitude: 106.7993735,
+        },
+        origin_postal_code: 12440,
+        destination_contact_name: 'stevanus miswari',
+        destination_contact_phone: '08170032123',
+        destination_contact_email: 'jon@test.com',
+        destination_address: 'Lebak Bulus MRT...',
+        destination_postal_code: 12950,
+        destination_note: 'Near the gas station',
+        destination_cash_proof_of_delivery: true,
+        destination_coordinate: {
+          latitude: -6.28927,
+          longitude: 106.77492000000007,
+        },
+        courierName: 'grab',
+        courierService: '',
+        courier_insurance: 500000,
+        delivery_type: 'later',
+        delivery_date: '2024-09-24',
+        delivery_time: '12:00',
+        order_note: 'Please be careful',
+        metadata: {},
+        items: [
+          {
+            id: '5db7ee67382e185bd6a14608',
+            name: 'Black L',
+            image: '',
+            description: 'White Shirt',
+            value: 165000,
+            quantity: 1,
+            height: 10,
+            length: 10,
+            weight: 200,
+            width: 10,
+          },
+        ],
+      };
+
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(orderData),
+      };
+
+      const response = await fetch(`${baseUrl}${endpoint}`, requestOptions);
+      const responseData = await response.json();
+
+      alert(responseData);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const [modalText, setModalText] = useState('');
   return (
     <>
       <Card mb={5} boxShadow={'xs'}>
@@ -22,17 +162,60 @@ export default function CardNewOrderBa(props: IOrderDetailInvoice) {
                 </Button>
 
                 {/* SET WHAT DO YOU WANT TO DO WITH YOUR BUTTON HERE */}
-                <Link to={`http://wa.me`}>
-                  <Button
-                    bg={'transparent'}
-                    border={'1px solid #D5D5D5'}
-                    borderRadius={'full'}
-                    fontSize={'14px'}
-                  >
-                    Proses Pesanan
-                  </Button>
-                </Link>
+                <Button
+                  bg={'transparent'}
+                  border={'1px solid #D5D5D5'}
+                  borderRadius={'full'}
+                  fontSize={'14px'}
+                  onClick={() => {
+                    setModalText('Apakah sudah di pack dan siap dikirim?');
+                    onOpen();
+                  }}
+                >
+                  Proses Pesanan
+                </Button>
                 {/*  */}
+                {/* Modal */}
+                <Modal
+                  blockScrollOnMount={false}
+                  isOpen={isOpen}
+                  onClose={() => {
+                    setModalText('');
+                    onClose();
+                  }}
+                >
+                  <ModalOverlay />
+                  <ModalContent>
+                    <ModalHeader>Proses Pesanan</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                      <Text fontWeight="bold" mb="1rem">
+                        {modalText}
+                      </Text>
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button
+                        colorScheme="blue"
+                        mr={3}
+                        onClick={() => {
+                          setModalText('');
+                          onClose();
+                        }}
+                      >
+                        Cancel
+                      </Button>
+                      <Button
+                        variant="ghost"
+                        onClick={() => {
+                          afterpacking();
+                          onClose();
+                        }}
+                      >
+                        Selesai di Packing
+                      </Button>
+                    </ModalFooter>
+                  </ModalContent>
+                </Modal>
               </Flex>
               <Text my={1} fontSize={'14px'} color={'gray.400'} px={2}>
                 {props.invoiceNumber}
@@ -44,8 +227,13 @@ export default function CardNewOrderBa(props: IOrderDetailInvoice) {
                     w={'52px'}
                     h={'52px'}
                     display={'inline'}
+                    src={`${props.cart.cartItems.map(
+                      (a) => a.product.attachments[0].url
+                    )}`}
                     // src={`${props.cart.cartItems.map((a)=> a.product.attachments.map)}`}
+                    // src={``}
                     mt={3}
+                    mx={3}
                   />
                   <Text
                     mt={4}
@@ -58,7 +246,7 @@ export default function CardNewOrderBa(props: IOrderDetailInvoice) {
                   >
                     {props.cart.cartItems.map((a) => a.product.name)}
                     <Text color={'gray.400'} pb={3} fontWeight={'normal'}>
-                      1 Barang
+                      {props.cart.cartItems.map((a) => a.qty)} Barang
                     </Text>
                   </Text>
                 </Box>
@@ -75,7 +263,9 @@ export default function CardNewOrderBa(props: IOrderDetailInvoice) {
                     Rp{' '}
                     {props.cart.cartItems.map((a) =>
                       a.variantOption.variantOptionValues.map((b) => b.price)
-                    )}
+                    )}{' '}
+                    {' * '}
+                    {props.payment.amount}
                   </Text>
                 </Box>
               </Flex>
