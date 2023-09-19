2fb260fcda844dae4699b48843252ea44b648388
AnggaNaa <angga.ardiansyah056@gmail.com>
2023-09-19 11:21:55 +0700

    fix: stepper in file ModalInShipping

--- app/components/ModalInShipping.tsx
+++ app/components/ModalInShipping.tsx
@@ -18,13 +18,13 @@ import {
   Text,
   useSteps,
   useToast,
-} from "@chakra-ui/react";
-import { format } from "date-fns-tz";
-import { useState } from "react";
-import { BsCircleFill } from "react-icons/bs";
-import copy from "~/assets/DetailOrderIcon/copy.svg";
-import { UseBiteshipTrack } from "~/hooks/useBiteshipTrack";
-import { ITracking } from "~/interfaces/order/orderTracking";
+} from '@chakra-ui/react';
+import { format } from 'date-fns-tz';
+import { useState } from 'react';
+import { BsCircleFill } from 'react-icons/bs';
+import copy from '~/assets/DetailOrderIcon/copy.svg';
+import { UseBiteshipTrack } from '~/hooks/useBiteshipTrack';
+import type { ITracking } from '~/interfaces/order/orderTracking';
 
 export default function ModalInShipping(props: {
   isOpen: boolean;
@@ -34,7 +34,7 @@ export default function ModalInShipping(props: {
   const { trackingInfoArray, trackingInfo } = UseBiteshipTrack();
 
   const steps = trackingInfoArray;
-  console.log("step", steps);
+  console.log('step', steps);
 
   const { activeStep } = useSteps({
     index: 1,
@@ -44,6 +44,8 @@ export default function ModalInShipping(props: {
   const toast = useToast();
   const [, setCopied] = useState(false);
 
+  const stepCount = steps.length;
+  const stepHeight = 65;
 
   // useEffect(() => {
   //   steps;
@@ -55,11 +57,11 @@ export default function ModalInShipping(props: {
       setCopied(true);
 
       toast({
-        title: "Teks telah disalin ke clipboard!",
-        status: "success",
+        title: 'Teks telah disalin ke clipboard!',
+        status: 'success',
         duration: 1000,
         isClosable: true,
-        position: "top",
+        position: 'top',
       });
     });
   };
@@ -74,115 +76,115 @@ export default function ModalInShipping(props: {
           <ModalBody>
             <Box>
               <Box
-                width={"606px"}
-                height={"176px"}
-                padding={"10px"}
-                gap={"10px"}
-                display={"flex"}
+                width={'606px'}
+                height={'176px'}
+                padding={'10px'}
+                gap={'10px'}
+                display={'flex'}
               >
                 <Box
-                  display={"flex"}
-                  flexDirection={"column"}
-                  width={"288px"}
-                  height={"156px"}
+                  display={'flex'}
+                  flexDirection={'column'}
+                  width={'288px'}
+                  height={'156px'}
                   gap={3}
                 >
-                  <Box width={"288px"} height={"44px"} gap={1}>
+                  <Box width={'288px'} height={'44px'} gap={1}>
                     <Text
-                      fontSize={"14px"}
-                      fontWeight={"400"}
-                      lineHeight={"20px"}
-                      color={"#1D1D1D"}
+                      fontSize={'14px'}
+                      fontWeight={'400'}
+                      lineHeight={'20px'}
+                      color={'#1D1D1D'}
                     >
                       Kurir
                     </Text>
                     <Text
-                      fontSize={"14px"}
-                      fontWeight={"700"}
-                      lineHeight={"20px"}
-                      color={"#1D1D1D"}
+                      fontSize={'14px'}
+                      fontWeight={'700'}
+                      lineHeight={'20px'}
+                      color={'#1D1D1D'}
                     >
                       {trackingInfo?.courier.company}
                     </Text>
                   </Box>
-                  <Box width={"288px"} height={"44px"} gap={1}>
-                    <Box display={"flex"} gap={1}>
+                  <Box width={'288px'} height={'44px'} gap={1}>
+                    <Box display={'flex'} gap={1}>
                       <Text
-                        fontSize={"14px"}
-                        fontWeight={"400"}
-                        lineHeight={"20px"}
-                        color={"#1D1D1D"}
+                        fontSize={'14px'}
+                        fontWeight={'400'}
+                        lineHeight={'20px'}
+                        color={'#1D1D1D'}
                       >
                         No. Resi
                       </Text>
                       <Image
-                        height={"18px"}
-                        width={"18px"}
-                        justifyContent={"center"}
-                        alignItems={"center"}
+                        height={'18px'}
+                        width={'18px'}
+                        justifyContent={'center'}
+                        alignItems={'center'}
                         src={copy}
                         onClick={handleCopyClick}
-                        style={{ cursor: "pointer" }}
-                        color={"gray.900"}
+                        style={{ cursor: 'pointer' }}
+                        color={'gray.900'}
                       />
                     </Box>
                     <Text
-                      fontSize={"14px"}
-                      fontWeight={"700"}
-                      lineHeight={"20px"}
-                      color={"#1D1D1D"}
+                      fontSize={'14px'}
+                      fontWeight={'700'}
+                      lineHeight={'20px'}
+                      color={'#1D1D1D'}
                     >
                       {trackingInfo?.waybill_id}
                     </Text>
                   </Box>
-                  <Box width={"288px"} height={"44px"} gap={1}>
+                  <Box width={'288px'} height={'44px'} gap={1}>
                     <Text
-                      fontSize={"14px"}
-                      fontWeight={"400"}
-                      lineHeight={"20px"}
-                      color={"#1D1D1D"}
+                      fontSize={'14px'}
+                      fontWeight={'400'}
+                      lineHeight={'20px'}
+                      color={'#1D1D1D'}
                     >
                       Pengirim
                     </Text>
                     <Text
-                      fontSize={"14px"}
-                      fontWeight={"700"}
-                      lineHeight={"20px"}
-                      color={"#1D1D1D"}
+                      fontSize={'14px'}
+                      fontWeight={'700'}
+                      lineHeight={'20px'}
+                      color={'#1D1D1D'}
                     >
                       {trackingInfo?.origin.contact_name}
                     </Text>
                   </Box>
                 </Box>
                 <Box
-                  display={"flex"}
-                  flexDirection={"column"}
-                  width={"288px"}
-                  height={"86px"}
+                  display={'flex'}
+                  flexDirection={'column'}
+                  width={'288px'}
+                  height={'86px'}
                   gap={4}
                 >
-                  <Box width={"288px"} height={"44px"} gap={"4px"}>
+                  <Box width={'288px'} height={'44px'} gap={'4px'}>
                     <Text
-                      fontSize={"14px"}
-                      fontWeight={"400"}
-                      lineHeight={"20px"}
-                      color={"#1D1D1D"}
+                      fontSize={'14px'}
+                      fontWeight={'400'}
+                      lineHeight={'20px'}
+                      color={'#1D1D1D'}
                     >
                       Penerima
                     </Text>
                     <Text
-                      fontSize={"14px"}
-                      fontWeight={"700"}
-                      lineHeight={"20px"}
-                      color={"#1D1D1D"}
+                      fontSize={'14px'}
+                      fontWeight={'700'}
+                      lineHeight={'20px'}
+                      color={'#1D1D1D'}
                     >
                       {trackingInfo?.destination.contact_name}
                     </Text>
                     <Text
-                      fontSize={"14px"}
-                      fontWeight={"400"}
-                      lineHeight={"20px"}
-                      color={"#1D1D1D"}
+                      fontSize={'14px'}
+                      fontWeight={'400'}
+                      lineHeight={'20px'}
+                      color={'#1D1D1D'}
                     >
                       {trackingInfo?.destination.address}
                     </Text>
@@ -190,56 +192,90 @@ export default function ModalInShipping(props: {
                 </Box>
               </Box>
               <Box
-                width={"606px"}
-                height={"20px"}
-                padding={"10px"}
-                gap={"2px"}
-                display={"flex"}
+                width={'606px'}
+                height={'20px'}
+                padding={'10px'}
+                gap={'2px'}
+                display={'flex'}
               >
-                <Box width={"55px"} height={"20px"}>
+                <Box width={'55px'} height={'20px'}>
                   <Text
-                    fontSize={"16px"}
-                    fontWeight={"500"}
-                    lineHeight={"20px"}
-                    color={"#1D1D1D"}
+                    fontSize={'16px'}
+                    fontWeight={'500'}
+                    lineHeight={'20px'}
+                    color={'#1D1D1D'}
                   >
                     Status:
                   </Text>
                 </Box>
-                <Box width={"196px"} height={"20px"}>
+                <Box width={'196px'} height={'20px'}>
                   <Text
-                    fontSize={"16px"}
-                    fontWeight={"700"}
-                    lineHeight={"20px"}
-                    color={"#1D1D1D"}
+                    fontSize={'16px'}
+                    fontWeight={'700'}
+                    lineHeight={'20px'}
+                    color={'#1D1D1D'}
                   >
                     {trackingInfo?.status}
                   </Text>
                 </Box>
               </Box>
               <Box
-                width={"606px"}
+                width={'606px'}
                 // height={"300px"}
-                padding={" 20px 16px 0px 16px"}
+                padding={' 20px 16px 0px 16px'}
               >
                 <Stepper
-                  size={"sm"}
-                  border={"1px solid #E6E6E6"}
-                  borderRadius={"12px"}
+                  size={'sm'}
                   index={activeStep}
                   orientation="vertical"
-                  // height="110%"
-                  width={"100%"}
-                  gap="5"
-                  p={"16px"}
+                  width={'100%'}
+                  height={`${stepCount * stepHeight}px`}
+                  gap="0"
+                  border={'1px solid #E6E6E6'}
+                  padding={'var(--4, 16px)'}
+                  borderRadius={'var(--rounded-lg, 12px)'}
                 >
                   {steps.map((step: any, index: number) => (
                     <Step key={index}>
-                      <StepIndicator fontSize={"11px"}>
+                      <StepIndicator>
                         <StepStatus
-                          complete={<BsCircleFill />}
-                          incomplete={<BsCircleFill color="gray" />}
-                          active={<BsCircleFill color="gray" />}
+                          complete={
+                            <div
+                              style={{
+                                background:
+                                  index === 0 ? '#C5F8FF' : 'transparent',
+                                borderRadius: '50%',
+                                padding: '7px',
+                                display: 'inline-block',
+                              }}
+                            >
+                              <BsCircleFill size={'12px'} color="#0086B4" />
+                            </div>
+                          }
+                          incomplete={
+                            <div
+                              style={{
+                                background: '#F8F8F8',
+                                borderRadius: '50%',
+                                padding: '7px',
+                                display: 'inline-block',
+                              }}
+                            >
+                              <BsCircleFill size={'12px'} color="#D5D5D5" />
+                            </div>
+                          }
+                          active={
+                            <div
+                              style={{
+                                background: '#F8F8F8',
+                                borderRadius: '50%',
+                                padding: '7px',
+                                display: 'inline-block',
+                              }}
+                            >
+                              <BsCircleFill size={'12px'} color="#D5D5D5" />
+                            </div>
+                          }
                         />
                       </StepIndicator>
                       <Box flexShrink="0">
@@ -247,13 +283,13 @@ export default function ModalInShipping(props: {
                         <StepDescription>
                           {format(
                             new Date(step?.updated_at),
-                            "EEE, dd MMM yyyy - HH:mm zzzz", // Desired output format
-                            { timeZone: "Asia/Jakarta" } // Time zone (WIB)
+                            'EEE, dd MMM yyyy - HH:mm zzzz', // Desired output format
+                            { timeZone: 'Asia/Jakarta' } // Time zone (WIB)
                           )}
                         </StepDescription>
                       </Box>
 
-                      <StepSeparator />
+                      <StepSeparator style={{ background: '#E6E6E6' }} />
                     </Step>
                   ))}
                 </Stepper>
