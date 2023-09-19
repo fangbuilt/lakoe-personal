bf6f5d22b96a4a4429beaa92ec91f0d8eeaf1b8f
suryaa6666 <suryaelidanto@gmail.com>
2023-09-19 13:35:24 +0700

    feat: merge branch productList2 into dev2

--- app/interfaces/mailerlite.ts
+++ app/interfaces/mailerlite.ts
@@ -1,6 +1,12 @@
-export interface IMailerLite {
+export interface IPickingUp {
   email: string;
   fields?: object;
   groups?: Array<string>;
   status?: 'active' | 'unsubscribed' | 'unconfirmed' | 'bounced' | 'junk';
+}
+
+export interface IInputDroppingOff {
+  email: string;
+  name: string;
+  waybill: string;
 }
