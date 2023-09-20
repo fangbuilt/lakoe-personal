6502235f647578d669f3417707df567c545691d0
pujaayumiswari <payumiswari@gmail.com>
2023-09-20 23:37:14 +0700

    feat: merging neworder with dev2

--- app/components/CardNewOrderBa.tsx
+++ app/components/CardNewOrderBa.tsx
@@ -217,135 +217,129 @@ export default function CardNewOrderBa() {
   const [modalText, setModalText] = useState('');
   return (
     <>
-      <Card mb={5} boxShadow={'xs'}>
-        <Box>
-          <Box mt={5}>
-            <Box>
-              <Flex justifyContent={'space-between'} px={2}>
-                <Button
-                  bg={'#008F5D'}
-                  color={'white'}
-                  fontWeight={'bold'}
-                  colorScheme="red.500"
-                  size={'sm'}
-                  pointerEvents={'none'}
-                >
-                  Pesanan Baru
-                </Button>
-
-                {/* SET WHAT DO YOU WANT TO DO WITH YOUR BUTTON HERE */}
-                <Button
-                  bg={'transparent'}
-                  border={'1px solid #D5D5D5'}
-                  borderRadius={'full'}
-                  fontSize={'14px'}
-                  onClick={() => {
-                    setModalText('Apakah sudah di pack dan siap dikirim?');
-                    onOpen();
-                  }}
-                >
-                  Proses Pesanan
-                </Button>
-                {/*  */}
-                {/* Modal */}
-                <Modal
-                  blockScrollOnMount={false}
-                  isOpen={isOpen}
-                  onClose={() => {
-                    setModalText('');
-                    onClose();
-                  }}
-                >
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader>Proses Pesanan</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                      <Text fontWeight="bold" mb="1rem">
-                        {modalText}
-                      </Text>
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button
-                        colorScheme="blue"
-                        mr={3}
-                        onClick={() => {
-                          setModalText('');
-                          onClose();
-                        }}
-                      >
-                        Cancel
-                      </Button>
-                      <Button
-                        variant="ghost"
-                        onClick={() => {
-                          afterpacking();
-                          onClose();
-                        }}
-                      >
-                        Selesai di Packing
-                      </Button>
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
-              </Flex>
-              <Text my={1} fontSize={'14px'} color={'gray.400'} px={2}>
-                {props.map((c) => c.invoiceNumber)}
-              </Text>
-              <hr />
-              <Flex justifyContent={'space-between'}>
-                <Box display={'flex'} w={'80%'}>
-                  <Image
-                    w={'52px'}
-                    h={'52px'}
-                    display={'inline'}
-                    src={`${props.map((s) =>
-                      s.cart?.cartItems.map((a) =>
-                        a.product?.attachments.map((jancok) => jancok.url)
-                      )
-                    )}`}
-                    mt={3}
-                    mx={3}
-                  />
-                  <Text
-                    mt={4}
-                    id="fm500"
-                    fontSize={'16px'}
-                    textOverflow={'ellipsis'}
-                    overflow={'hidden'}
-                    whiteSpace={'nowrap'}
-                    fontWeight={'700'}
+      {props.map((props) => (
+        <Card mb={5} boxShadow={'xs'}>
+          <Box>
+            <Box mt={5}>
+              <Box>
+                <Flex justifyContent={'space-between'} px={2}>
+                  <Button
+                    bg={'#008F5D'}
+                    color={'white'}
+                    fontWeight={'bold'}
+                    colorScheme="red.500"
+                    size={'sm'}
+                    pointerEvents={'none'}
                   >
-                    {props.map((c) =>
-                      c.cart?.cartItems.map((a) => a.product?.name)
-                    )}
-                    <Text color={'gray.400'} pb={3} fontWeight={'normal'}>
-                      {props.map((aw) => aw.cart?.cartItems.map((a) => a.qty))}{' '}
-                      Barang
+                    Pesanan Baru
+                  </Button>
+
+                  {/* SET WHAT DO YOU WANT TO DO WITH YOUR BUTTON HERE */}
+                  <Button
+                    bg={'transparent'}
+                    border={'1px solid #D5D5D5'}
+                    borderRadius={'full'}
+                    fontSize={'14px'}
+                    onClick={() => {
+                      setModalText('Apakah sudah di pack dan siap dikirim?');
+                      onOpen();
+                    }}
+                  >
+                    Proses Pesanan
+                  </Button>
+                  {/*  */}
+                  {/* Modal */}
+                  <Modal
+                    blockScrollOnMount={false}
+                    isOpen={isOpen}
+                    onClose={() => {
+                      setModalText('');
+                      onClose();
+                    }}
+                  >
+                    <ModalOverlay />
+                    <ModalContent>
+                      <ModalHeader>Proses Pesanan</ModalHeader>
+                      <ModalCloseButton />
+                      <ModalBody>
+                        <Text fontWeight="bold" mb="1rem">
+                          {modalText}
+                        </Text>
+                      </ModalBody>
+                      <ModalFooter>
+                        <Button
+                          colorScheme="blue"
+                          mr={3}
+                          onClick={() => {
+                            setModalText('');
+                            onClose();
+                          }}
+                        >
+                          Cancel
+                        </Button>
+                        <Button
+                          variant="ghost"
+                          onClick={() => {
+                            afterpacking();
+                            onClose();
+                          }}
+                        >
+                          Selesai di Packing
+                        </Button>
+                      </ModalFooter>
+                    </ModalContent>
+                  </Modal>
+                </Flex>
+                <Text my={1} fontSize={'14px'} color={'gray.400'} px={2}>
+                  {props.invoiceNumber}
+                </Text>
+                <hr />
+                <Flex justifyContent={'space-between'}>
+                  <Box display={'flex'} w={'80%'}>
+                    <Image
+                      w={'52px'}
+                      h={'52px'}
+                      display={'inline'}
+                      src={`${props.cart?.cartItems.map((a) =>
+                        a.product?.attachments.map((jancok) => jancok.url)
+                      )}`}
+                      mt={3}
+                      mx={3}
+                    />
+                    <Text
+                      mt={4}
+                      id="fm500"
+                      fontSize={'16px'}
+                      textOverflow={'ellipsis'}
+                      overflow={'hidden'}
+                      whiteSpace={'nowrap'}
+                      fontWeight={'700'}
+                    >
+                      {props.cart?.cartItems.map((a) => a.product?.name)}
+                      <Text color={'gray.400'} pb={3} fontWeight={'normal'}>
+                        {props.cart?.cartItems.map((a) => a.qty)} Barang
+                      </Text>
                     </Text>
-                  </Text>
-                </Box>
-                <Box mt={4} w={'15%'}>
-                  <Flex gap={1}>
-                    <Text color={'#909090'} fontSize={'14px'}>
-                      Total
+                  </Box>
+                  <Box mt={4} w={'15%'}>
+                    <Flex gap={1}>
+                      <Text color={'#909090'} fontSize={'14px'}>
+                        Total
+                      </Text>
+                      <Text color={'#909090'} fontSize={'14px'}>
+                        Belanja
+                      </Text>
+                    </Flex>
+                    <Text fontWeight={'bold'} fontSize={'14px'}>
+                      Rp {props.cart?.cartItems.map((a) => a.price * a.qty)}
                     </Text>
-                    <Text color={'#909090'} fontSize={'14px'}>
-                      Belanja
-                    </Text>
-                  </Flex>
-                  <Text fontWeight={'bold'} fontSize={'14px'}>
-                    Rp{' '}
-                    {props.map((ay) =>
-                      ay.cart?.cartItems.map((a) => a.price * a.qty)
-                    )}
-                  </Text>
-                </Box>
-              </Flex>
+                  </Box>
+                </Flex>
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </Card>
+        </Card>
+      ))}
     </>
   );
 }
